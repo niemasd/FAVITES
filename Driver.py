a43c1011fd8c7d32642b830bbef105d42f38235a
Niema Moshiri <niemamoshiri@gmail.com>
2016-11-27 23:06:10 -0800

    Updated driver

--- Driver.py
+++ Driver.py
@@ -7,6 +7,12 @@ Niema Moshiri 2016
 import argparse                           # to parse user arguments
 from ContactNetwork import ContactNetwork # ContactNetwork module abstract class
 
+def printMessage():
+    print("/---------------------------------------------------------------------\\")
+    print("| FAVITES - FrAmework for VIral Transmission and Evolution Simulation |")
+    print("|                        Moshiri & Mirarab 2016                       |")
+    print("\\---------------------------------------------------------------------/")
+
 def parseArgs():
     '''
     Parse user arguments
@@ -14,6 +20,7 @@ def parseArgs():
     Returns
     -------
     user_input : list of user inputs
+        * ``user_input['contact_network']'': Input contact network (edge list)
 
     '''
 
@@ -39,24 +46,35 @@ def parseArgs():
 
     print()
 
+    # read input data
+    print("=== Input Data ===")
+    user_input = {}
+
     # read in Contact Network
     print("Reading contact network from: ", end='')
-    ContactNetwork_edgeList = []
+    user_input['contact_network'] = []
     if args.ContactNetworkFile == 'stdin':
         import sys
-        ContactNetwork_edgeList = sys.stdin.readlines()
+        user_input['contact_network'] = [i.strip() for i in sys.stdin if len(i.strip()) > 0]
         print('standard input')
     else:
-        ContactNetwork_edgeList = open(args.ContactNetworkFile).readlines()
+        user_input['contact_network'] = open(args.ContactNetworkFile).readlines()
         print(args.ContactNetworkFile)
+
+    # return input data
+    return user_input
 
 if __name__ == "__main__":
     '''
     Simulation driver
     '''
 
-    # parse user arguments
-    parseArgs()
+    # print author message
+    printMessage()
+    print()
 
-    # create ContactNetwork object
-    #contact_network = module_ContactNetwork()
+    # parse user arguments
+    user_input = parseArgs()
+
+    # create ContactNetwork object from input contact network edge list
+    contact_network = module_ContactNetwork(user_input['contact_network'])
