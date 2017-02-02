70535a7247a82796f0608ca7d385422c7b29fbf0
Niema Moshiri <niemamoshiri@gmail.com>
2017-02-01 21:35:53 -0800

    Added Sequencing_ARTSOLiDAmpliconMatePair module

--- modules/Sequencing_ARTSOLiDAmpliconMatePair.py
+++ modules/Sequencing_ARTSOLiDAmpliconMatePair.py
@@ -2,7 +2,7 @@
 '''
 Niema Moshiri 2016
 
-"Sequencing" module, using ART to simulate SOLiD reads (amplicon single-end, F3)
+"Sequencing" module, using ART to simulate SOLiD reads (amplicon mate-pair, F3-R3)
 '''
 from Sequencing import Sequencing
 import FAVITES_GlobalContext as GC
@@ -12,7 +12,7 @@ from os import getcwd
 from os import makedirs
 from os import chdir
 
-class Sequencing_ARTSOLiDAmpliconSingleEnd(Sequencing):
+class Sequencing_ARTSOLiDAmpliconMatePair(Sequencing):
     def init():
         GC.out_dir = expanduser(GC.out_dir)
         GC.art_SOLiD_options = [i.strip() for i in GC.art_SOLiD_options.strip().split()]
@@ -20,11 +20,11 @@ class Sequencing_ARTSOLiDAmpliconSingleEnd(Sequencing):
         assert GC.art_SOLiD_len_read <= 75, "Maximum ART SOLiD read length is 75"
 
     def introduce_sequencing_error(node):
-        command = [GC.art_SOLiD_path] + GC.art_SOLiD_options + ['-A','s']
+        command = [GC.art_SOLiD_path] + GC.art_SOLiD_options + ['-A','m']
         command.append(GC.out_dir + "/error_free_files/sequence_data/seqs_" + node.get_name() + ".fasta")
         command.append(node.get_name())
         command.append(str(GC.art_SOLiD_len_read))
-        command.append(str(GC.art_SOLiD_reads_per_amplicon))
+        command.append(str(GC.art_SOLiD_read_pairs_per_amplicon))
         orig_dir = getcwd()
         chdir(GC.out_dir)
         makedirs("error_prone_files/sequence_data", exist_ok=True)
