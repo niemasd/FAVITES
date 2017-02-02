d36035629a8cf43f2b3b42d5beb51fee5ddc7b80
Niema Moshiri <niemamoshiri@gmail.com>
2017-02-01 18:27:14 -0800

    Added Sequencing_ART454PairedEnd module

--- modules/Sequencing_ART454PairedEnd.py
+++ modules/Sequencing_ART454PairedEnd.py
@@ -2,7 +2,7 @@
 '''
 Niema Moshiri 2016
 
-"Sequencing" module, using ART to simulate Roche 454 reads (single-end)
+"Sequencing" module, using ART to simulate Roche 454 reads (paired-end)
 '''
 from Sequencing import Sequencing
 import FAVITES_GlobalContext as GC
@@ -12,7 +12,7 @@ from os import getcwd
 from os import makedirs
 from os import chdir
 
-class Sequencing_ART454SingleEnd(Sequencing):
+class Sequencing_ART454PairedEnd(Sequencing):
     def init():
         GC.out_dir = expanduser(GC.out_dir)
         GC.art_454_args = [i.strip() for i in GC.art_454_args.strip().split()]
@@ -23,6 +23,8 @@ class Sequencing_ART454SingleEnd(Sequencing):
         command.append(GC.out_dir + "/error_free_files/sequence_data/seqs_" + node.get_name() + ".fasta")
         command.append(node.get_name())
         command.append(str(GC.art_454_fold_coverage))
+        command.append(str(GC.art_454_mean_frag_len))
+        command.append(str(GC.art_454_std_dev))
         orig_dir = getcwd()
         chdir(GC.out_dir)
         makedirs("error_prone_files/sequence_data", exist_ok=True)
