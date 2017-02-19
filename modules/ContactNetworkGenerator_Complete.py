eb9ae7eaeb2a344ffde731811929ee227e4e2946
Niema Moshiri <niemamoshiri@gmail.com>
2017-02-18 19:51:57 -0800

    Added ContactNetworkGenerator_Complete module

---
+++ modules/ContactNetworkGenerator_Complete.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2016
+
+"ContactNetworkGenerator" module, creating a complete graph of n nodes
+'''
+from ContactNetworkGenerator import ContactNetworkGenerator
+import FAVITES_GlobalContext as GC
+from os.path import expanduser
+from networkx import complete_graph
+
+class ContactNetworkGenerator_Complete(ContactNetworkGenerator):
+    def init():
+        assert isinstance(GC.num_cn_nodes, int), "num_cn_nodes must be an integer"
+        assert GC.num_cn_nodes >= 2, "Contact network must have at least 2 nodes"
+
+    def get_edge_list():
+        cn = complete_graph(GC.num_cn_nodes)
+        out = GC.nx2favites(cn, 'u')
+        f = open(expanduser(GC.out_dir + "/contact_network.txt"),'w')
+        f.write('\n'.join(out))
+        f.close()
+        return out
