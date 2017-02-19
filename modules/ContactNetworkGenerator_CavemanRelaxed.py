41552f0c9f00b3232ffedfb8418ff54c2aa584f4
Niema Moshiri <niemamoshiri@gmail.com>
2017-02-18 20:27:21 -0800

    Added ContactNetworkGenerator_CavemanRelaxed module

---
+++ modules/ContactNetworkGenerator_CavemanRelaxed.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2016
+
+"ContactNetworkGenerator" module, creating a Relaxed Caveman graph
+'''
+from ContactNetworkGenerator import ContactNetworkGenerator
+import FAVITES_GlobalContext as GC
+from os.path import expanduser
+from networkx import relaxed_caveman_graph
+
+class ContactNetworkGenerator_CavemanRelaxed(ContactNetworkGenerator):
+    def init():
+        assert isinstance(GC.cave_num_cliques, int), "cave_num_cliques must be an integer"
+        assert GC.cave_num_cliques > 0, "Must have at least 1 clique"
+        assert isinstance(GC.cave_clique_size, int), "cave_clique_size must be an integer"
+        assert GC.cave_clique_size > 0, "Cliques must contain at least 1 node"
+        GC.cave_prob = float(GC.cave_prob)
+        assert GC.cave_prob >= 0 and GC.cave_prob <= 1, "cave_prob must be between 0 and 1"
+
+    def get_edge_list():
+        cn = relaxed_caveman_graph(GC.cave_num_cliques, GC.cave_clique_size, GC.cave_prob)
+        out = GC.nx2favites(cn, 'u')
+        f = open(expanduser(GC.out_dir + "/contact_network.txt"),'w')
+        f.write('\n'.join(out))
+        f.close()
+        return out
