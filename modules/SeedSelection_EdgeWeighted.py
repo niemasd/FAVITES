deb26d217455a068a974fe1867cf68f224b851b4
Niema Moshiri <niemamoshiri@gmail.com>
2017-03-26 15:53:01 -0700

    Added SeedSelection_EdgeWeighted module

---
+++ modules/SeedSelection_EdgeWeighted.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2016
+
+"SeedSelection" module, where the probability at a node is chosen is weighted by
+its degree
+'''
+from SeedSelection import SeedSelection
+import FAVITES_GlobalContext as GC
+from random import choice
+
+class SeedSelection_EdgeWeighted(SeedSelection):
+    def init():
+        GC.num_seeds = int(GC.num_seeds)
+        assert GC.num_seeds >= 1, "Must have at least 1 seed node"
+
+    def select_seeds():
+        edges = [edge for edge in GC.contact_network.edges_iter()]
+        seed_nodes = set()
+        while len(seed_nodes) < GC.num_seeds:
+            edge = choice(edges)
+            seed_nodes.add(choice((edge.get_from(), edge.get_to())))
+        return list(seed_nodes)
