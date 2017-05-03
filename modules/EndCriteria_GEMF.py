1c863cacbd119d8982867bfda89c688ece8f1a1c
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-03 15:52:57 -0700

    Output README for GEMF data

--- modules/EndCriteria_GEMF.py
+++ modules/EndCriteria_GEMF.py
@@ -8,11 +8,24 @@ Niema Moshiri 2016
 from EndCriteria import EndCriteria
 import modules.FAVITES_ModuleFactory as MF
 import FAVITES_GlobalContext as GC
+from os import chdir,getcwd
+
+gemf_general_help = "This file contains general helpful information about the GEMF output."
+gemf_output_txt_help = '''=== output.txt ===
+This is the main file of interest. It contains the GEMF simulation output. The columns of the output file are as follows (in the exact order):
+* Time of event
+* Total rate
+* Node that was infected
+* Previous state of the node
+* New state of node
+* Number of nodes in each state (one column per state)
+* Comma-delimited lists of inducer nodes from each state (one state per list)'''
 
 class EndCriteria_GEMF(EndCriteria):
     def init():
         assert "GEMF" in str(MF.modules['TransmissionNodeSample']), "Must use a GEMF TransmissionNodeSample module"
         assert "GEMF" in str(MF.modules['TransmissionTimeSample']), "Must use a GEMF TransmissionTimeSample module"
+        GC.gemf_out_dir = (GC.out_dir + '/GEMF_output').replace('//','/')
 
     def done():
         return GC.transmission_num == len(GC.transmission_file)
@@ -21,4 +34,9 @@ class EndCriteria_GEMF(EndCriteria):
         return not EndCriteria_TransmissionFile.done()
 
     def finalize_time():
-        pass
+        # write GEMF output README for user
+        f = open(GC.gemf_out_dir + '/README.TXT', 'w')
+        f.write(gemf_general_help)
+        f.write('\n\n')
+        f.write(gemf_output_txt_help)
+        f.close()
