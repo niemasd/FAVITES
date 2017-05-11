723dfbd3db165abcc30a4bec45f14481baccdabf
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-10 19:52:30 -0700

    Added Nucleotide Model

---
+++ modules/SequenceEvolution_NucleotideModelPyvolve.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2016
+
+"SequenceEvolution" module, implemented using a Nucleotide Model with Pyvolve.
+'''
+from SequenceEvolution import SequenceEvolution
+import FAVITES_GlobalContext as GC
+import modules.FAVITES_ModuleFactory as MF
+from os import makedirs
+
+class SequenceEvolution_NucleotideModelPyvolve(SequenceEvolution):
+    def init():
+        try:
+            global pyvolve
+            import pyvolve
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading Pyvolve. Install with: pip3 install pyvolve"
+        # config validity checks
+        custom_model_params = {}
+        if isinstance(GC.nuc_kappa, str):
+            GC.nuc_kappa = GC.nuc_kappa.strip()
+            if len(GC.nuc_kappa) != 0:
+                custom_model_params['kappa'] = float(GC.nuc_kappa)
+        else:
+            custom_model_params['kappa'] = float(GC.nuc_kappa)
+        assert isinstance(GC.nuc_frequencies_dictionary, dict), "Specified nuc_frequencies_dictionary is not a dictionary"
+        if len(GC.nuc_frequencies_dictionary) != 0:
+            for key in GC.nuc_frequencies_dictionary:
+                assert key in {'A','C','G','T'}, "%s is not a valid codon for nuc_frequencies_dictionary. Only DNA nucleotides (A, C, G, or T)"
+            assert abs(sum(GC.nuc_frequencies_dictionary.values()) - 1) < 0.000000001, "Frequencies in nuc_frequencies_dictionary must sum to 1"
+            custom_model_params['state_freqs'] = GC.nuc_frequencies_dictionary
+        assert isinstance(GC.nuc_mutation_rates_dictionary, dict), "Specified nuc_mutation_rates_dictionary is not a dictionary"
+        if len(GC.nuc_mutation_rates_dictionary) != 0:
+            custom_model_params['mu'] = GC.nuc_mutation_rates_dictionary
+        assert not ('kappa' in custom_model_params and 'nuc_mutation_rates_dictionary' in custom_model_params), "Cannot use custom values for both nuc_kappa and nuc_mutation_rates_dictionary: only one of the two"
+
+        # set up Pyvolve
+        if len(custom_model_params) == 0:
+            GC.pyvolve_model = pyvolve.Model("nucleotide")
+        else:
+            GC.pyvolve_model = pyvolve.Model("nucleotide", custom_model_params)
+
+    def evolve_to_current_time(node):
+        pass
+
+    def finalize():
+        makedirs("pyvolve_output")
+        label_to_node = MF.modules['TreeNode'].label_to_node()
+        roots = [root for root in GC.root_viruses]
+        for root in roots:
+            label = root.get_label()
+            tree = pyvolve.read_tree(tree=root.newick())
+            partition = pyvolve.Partition(models=GC.pyvolve_model, root_sequence=root.get_seq())
+            evolver = pyvolve.Evolver(partitions=partition, tree=tree)
+            ratefile = "pyvolve_output/" + label + "_ratefile.txt" # set each to None to not generate these files
+            infofile = "pyvolve_output/" + label + "_infofile.txt"
+            seqfile  = "pyvolve_output/" + label + "_seqfile.fasta"
+            evolver(ratefile=ratefile, infofile=infofile, seqfile=seqfile)
+            seqs = evolver.get_sequences() # use anc=True to get internal sequences as well
+            for label in seqs:
+                label_to_node[label].set_seq(seqs[label])
