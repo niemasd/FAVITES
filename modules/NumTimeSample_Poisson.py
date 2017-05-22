102154607b211b54ea7a6ce5ddb376625ba36719
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-22 11:38:49 -0700

    Added NumTimeSample_Poisson module

---
+++ modules/NumTimeSample_Poisson.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2017
+
+"NumTimeSample" module, where each infected individual in the contact network is
+sampled a number of times that is sampled from a Poisson distribution.
+'''
+from NumTimeSample import NumTimeSample
+import FAVITES_GlobalContext as GC
+
+class NumTimeSample_Poisson(NumTimeSample):
+    def init():
+        try:
+            global poisson
+            from numpy.random import poisson
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading NumPy. Install with: pip3 install numpy"
+        GC.num_time_sample_lambda = float(GC.num_time_sample_lambda)
+        assert GC.num_time_sample_lambda >= 0, "num_time_sample_lambda must be at least 0"
+
+    def sample_num_times(node):
+        if node.get_first_infection_time() is not None:
+            return poisson(lam=GC.num_time_sample_lambda)
+        else:
+            return 0
