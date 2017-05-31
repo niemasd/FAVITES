0bfdb3e7260ce2684968543f9e3dfd8484704764
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-30 23:23:21 -0700

    Fixed for multiple sampling

--- modules/Sequencing_ARTSOLiDAmpliconPairedEnd.py
+++ modules/Sequencing_ARTSOLiDAmpliconPairedEnd.py
@@ -8,9 +8,7 @@ from Sequencing import Sequencing
 import FAVITES_GlobalContext as GC
 from subprocess import call
 from os.path import expanduser
-from os import getcwd
-from os import makedirs
-from os import chdir
+from os import getcwd,makedirs,chdir,listdir
 
 class Sequencing_ARTSOLiDAmpliconPairedEnd(Sequencing):
     def init():
@@ -21,19 +19,21 @@ class Sequencing_ARTSOLiDAmpliconPairedEnd(Sequencing):
         assert GC.art_SOLiD_len_read_F5 <= 75, "Maximum ART SOLiD read length is 75"
 
     def introduce_sequencing_error(node):
-        command = [GC.art_SOLiD_path] + GC.art_SOLiD_options + ['-A','p']
-        command.append(GC.out_dir + "/error_free_files/sequence_data/seqs_" + node.get_name() + ".fasta")
-        command.append(node.get_name())
-        command.append(str(GC.art_SOLiD_len_read_F3))
-        command.append(str(GC.art_SOLiD_len_read_F5))
-        command.append(str(GC.art_SOLiD_read_pairs_per_amplicon))
         orig_dir = getcwd()
         chdir(GC.out_dir)
         makedirs("error_prone_files/sequence_data", exist_ok=True)
         chdir("error_prone_files/sequence_data")
-        try:
-            call(command, stdout=open("log_" + node.get_name() + ".txt", 'w'))
-        except FileNotFoundError:
-            chdir(GC.START_DIR)
-            assert False, "art_SOLiD executable was not found: %s" % GC.art_SOLiD_path
+        for filename in listdir(GC.out_dir + "/error_free_files/sequence_data"):
+            if filename.split('_')[1][1:] == node.get_name():
+                command = [GC.art_SOLiD_path] + GC.art_SOLiD_options + ['-A','p']
+                command.append(GC.out_dir + "/error_free_files/sequence_data/" + filename)
+                command.append(filename[:-6])
+                command.append(str(GC.art_SOLiD_len_read_F3))
+                command.append(str(GC.art_SOLiD_len_read_F5))
+                command.append(str(GC.art_SOLiD_read_pairs_per_amplicon))
+                try:
+                    call(command, stdout=open("log_" + filename[5:-6] + ".txt", 'w'))
+                except FileNotFoundError:
+                    chdir(GC.START_DIR)
+                    assert False, "art_SOLiD executable was not found: %s" % GC.art_SOLiD_path
         chdir(orig_dir)
