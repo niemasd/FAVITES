2ff46d7fa11fa76b4d477f42dea48d5fbea2c8e5
Niema Moshiri <niemamoshiri@gmail.com>
2017-05-31 20:46:30 -0700

    Added safety check

--- modules/TimeSample_End.py
+++ modules/TimeSample_End.py
@@ -7,10 +7,11 @@ at the end time, and no sample times are returned for all other nodes
 '''
 from TimeSample import TimeSample
 import FAVITES_GlobalContext as GC
+import modules.FAVITES_ModuleFactory as MF
 
 class TimeSample_End(TimeSample):
     def init():
-        pass
+        assert "NumTimeSample_Once" in str(MF.modules['NumTimeSample']), "Must use NumTimeSample_Once module"
 
     def sample_times(node, num_times):
         if node.is_infected():
