133834b446519a45a7eb88cb6b8e4816e633248f
Niema Moshiri <niemamoshiri@gmail.com>
2017-06-30 13:24:25 -0700

    test

--- modules/Driver_Default.py
+++ modules/Driver_Default.py
@@ -27,6 +27,7 @@ class Driver_Default(Driver):
 
     def init():
         GC.out_dir = expanduser(GC.out_dir)
+        print(GC.VERBOSE)
         print(GC.out_dir_print)
         if not hasattr(GC,'out_dir_print'):
             GC.out_dir_print = GC.out_dir
