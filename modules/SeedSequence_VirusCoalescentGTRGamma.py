41d1d286197dabc9c3e8c1be9b047e36240ae349
Niema Moshiri <niemamoshiri@gmail.com>
2017-08-16 17:40:26 -0700

    Fixed seq-gen error log output directory

--- modules/SeedSequence_VirusCoalescentGTRGamma.py
+++ modules/SeedSequence_VirusCoalescentGTRGamma.py
@@ -44,8 +44,8 @@ class SeedSequence_VirusCoalescentGTRGamma(SeedSequence):
             try:
                 seqgen_out = check_output(command, stdin=open(seqgen_file), stderr=open(OUT_FOLDER + '/log_seqgen.txt','w')).decode('ascii')
             except CalledProcessError as e:
-                chdir(GC.START_DIR)
                 f = open('error.log','w'); f.write(str(e)); f.close()
+                chdir(GC.START_DIR)
                 assert False, "Seq-Gen encountered an error"
             GC.seed_sequences = {line.split()[-1].strip() for line in seqgen_out.splitlines()[1:]}
         return GC.seed_sequences.pop()
--- modules/SequenceEvolution_SeqGen.py
+++ modules/SequenceEvolution_SeqGen.py
@@ -57,8 +57,8 @@ class SequenceEvolution_SeqGen(SequenceEvolution):
             try:
                 seqgen_out = check_output(command, stdin=open(label+'.txt'), stderr=open('log_'+label+'.txt','w')).decode('ascii')
             except CalledProcessError as e:
-                chdir(GC.START_DIR)
                 f = open('seqgen.err','w'); f.write(str(e)); f.close()
+                chdir(GC.START_DIR)
                 assert False, "Seq-Gen encountered an error"
 
             # store leaf sequences in GlobalContext
