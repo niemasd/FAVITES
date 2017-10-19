48e746e0cfbfa1f99eed913e1ba993f3317e79e2
Niema Moshiri <a1moshir@debruijn.ucsd.edu>
2017-10-18 22:04:33 -0700

    Switched to dendropy to simplify

--- modules/TreeUnit_LogNormal.py
+++ modules/TreeUnit_LogNormal.py
@@ -20,24 +20,19 @@ class TreeUnit_LogNormal(TreeUnit):
             from os import chdir
             chdir(GC.START_DIR)
             assert False, "Error loading NumPy. Install with: pip3 install numpy"
+        try:
+            global dendropy
+            import dendropy
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading DendroPy. Install with: pip3 install dendropy"
         GC.tree_rate_mean = float(GC.tree_rate_mean)
         GC.tree_rate_stdev = float(GC.tree_rate_stdev)
         assert GC.tree_rate_stdev > 0, "tree_rate_stdev must be positive"
 
     def time_to_mutation_rate(tree):
-        parts = tree.split(':')
-        for i in range(1,len(parts)):
-            try: # branch length is just before a comma
-                ind = parts[i].index(',')
-                parts[i] = "%f%s" % (float(parts[i][:ind])*lognormal(mean=GC.tree_rate_mean,sigma=GC.tree_rate_stdev), parts[i][ind:])
-            except ValueError: # branch length is just before a right parenthesis
-                try:
-                    ind = parts[i].index(')')
-                    parts[i] = "%f%s" % (float(parts[i][:ind])*lognormal(mean=GC.tree_rate_mean,sigma=GC.tree_rate_stdev), parts[i][ind:])
-                except ValueError: # branch length is just before the semicolon (root)
-                    try:
-                        ind = parts[i].index(';')
-                        parts[i] = "%f%s" % (float(parts[i][:ind])*lognormal(mean=GC.tree_rate_mean,sigma=GC.tree_rate_stdev), parts[i][ind:])
-                    except:
-                        assert False, "Failed to parse branch length from tree substring: %s" % parts[i]
-        return ':'.join(parts)
+        t = dendropy.Tree.get(data=tree,schema='newick')
+        for edge in t.preorder_edge_iter():
+            edge.length *= lognormal(mean=GC.tree_rate_mean,sigma=GC.tree_rate_stdev)
+        return str(t) + ';'
