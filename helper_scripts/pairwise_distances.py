2c55b6fbbbc69412f1b0733069b35e1ce8d9e294
Niema Moshiri <niemamoshiri@gmail.com>
2017-12-19 14:06:48 -0800

    Added script to compute pairwise leaf distances from tree

---
+++ helper_scripts/pairwise_distances.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Compute the pairwise distances between leaves of a given phylogenetic tree.
+'''
+
+if __name__ == "__main__":
+    # parse args
+    from sys import stdout; import argparse
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-t', '--tree', required=True, type=argparse.FileType('r'), help="Tree File")
+    parser.add_argument('-s', '--schema', required=False, type=str, default="newick", help="Tree File Schema")
+    parser.add_argument('-o', '--output', required=False, type=argparse.FileType('r'), default=stdout, help="Output")
+    args = parser.parse_args()
+
+    # load tree and compute distances
+    from dendropy import Tree
+    t = Tree.get(file=args.tree, schema=args.schema.strip())
+    d = t.phylogenetic_distance_matrix()
+    print(",%s" % ','.join([str(e)[1:-1] for e in t.taxon_namespace]))
+    for t1 in t.taxon_namespace:
+        print("%s,%s" % (str(t1)[1:-1], ','.join([str(d(t1,t2)) for t2 in t.taxon_namespace])))
