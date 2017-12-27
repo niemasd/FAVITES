ab4506b2d9683ff173c238e36063e96d94e988e9
Niema Moshiri <niemamoshiri@gmail.com>
2017-12-27 14:28:09 -0800

    Added post validation script to compare contact networks

--- post_validation/common.py
+++ post_validation/common.py
@@ -33,3 +33,26 @@ def readFASTQ(stream):
         assert name is not None and len(seq) != 0, "Malformed FASTQ"
     seqs[name] = (seq,qual)
     return seqs
+
+# return a list of vertex (out)degrees from a given FAVITES-format contact network file
+def degrees(f):
+    d = {}
+    for line in f:
+        if line[0] == '#' or len(line.strip()) == 0:
+            continue
+        parts = line.split()
+        assert len(parts) in {3,5} and parts[0] in {'NODE','EDGE'}, "Malformed contact network file"
+        if parts[0] == 'NODE':
+            assert len(parts) == 3, "Malformed contact network"
+            assert parts[1] not in d, "Duplicate node: %s" % parts[1]
+            d[parts[1]] = 0
+        else:
+            assert len(parts) == 5, "Malformed contact network"
+            u,v,du = parts[1],parts[2],parts[4]
+            assert du in {'d','u'}, "Malformed contact network"
+            assert u in d, "Unexpected node: %s" % u
+            assert v in d, "Unexpected node: %s" % v
+            d[u] += 1
+            if du == 'u':
+                d[v] += 1
+    return list(d.values())
---
+++ post_validation/compare_contact_networks.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+'''
+Compare a given simulated contact network against a given realistic reference
+contact network.
+'''
+from common import avg,degrees
+from numpy import std
+from os.path import isfile
+from scipy.stats import ks_2samp
+from subprocess import check_output
+import argparse
+
+# parse user args
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-r', '--ref', required=True, type=str, help="Reference Contact Network")
+parser.add_argument('-s', '--sim', required=True, type=str, help="Simulated Contact Network")
+args = parser.parse_args()
+assert isfile(args.ref), "ERROR: Invalid file: %s" % args.ref
+assert isfile(args.sim), "ERROR: Invalid file: %s" % args.sim
+
+# load data from contact networks
+ref_degrees = degrees(open(args.ref))
+sim_degrees = degrees(open(args.sim))
+
+# perform analyses
+print("Analysis\tReference Contact Network\tSimulated Contact Network\tTest Statistic\tp-value")
+print("Average Degree\t%g\t%g\tNA\tNA" % (avg(ref_degrees),avg(sim_degrees)))
+print("Standard Deviation Degree\t%g\t%g\tNA\tNA" % (std(ref_degrees),std(sim_degrees)))
+print("Kolmogorov-Smirnov Test Degree\tNA\tNA\t%g\t%g" % ks_2samp(ref_degrees,sim_degrees))
