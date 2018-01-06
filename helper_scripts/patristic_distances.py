f9c20b35c7659fafe0f36c32a577a67d072dec9a
Niema Moshiri <niemamoshiri@gmail.com>
2018-01-06 15:42:06 -0800

    Allow strings other than 0 for self-distances

--- helper_scripts/patristic_distances.py
+++ helper_scripts/patristic_distances.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--tree', required=True, type=argparse.FileType('r'), help="Tree File")
     parser.add_argument('-s', '--schema', required=False, type=str, default="newick", help="Tree File Schema")
     parser.add_argument('-o', '--output', required=False, type=argparse.FileType('w'), default=stdout, help="Output File")
+    parser.add_argument('-d', '--self_distance', required=False, type=str, default="0", help="String to Report Self-Distance (diagonal)")
     args,unknown = parser.parse_known_args()
 
     # load tree and compute distances
@@ -22,4 +23,4 @@ if __name__ == "__main__":
     d = t.phylogenetic_distance_matrix()
     args.output.write(",%s\n" % ','.join([str(e)[1:-1] for e in t.taxon_namespace]))
     for t1 in t.taxon_namespace:
-        args.output.write("%s,%s\n" % (str(t1)[1:-1], ','.join([str(d(t1,t2)) for t2 in t.taxon_namespace])))
+        args.output.write("%s,%s\n" % (str(t1)[1:-1], ','.join([str(d(t1,t2)) if t1 != t2 else args.self_distance for t2 in t.taxon_namespace])))
