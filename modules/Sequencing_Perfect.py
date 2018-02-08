bbac8f0282137df14e4167b8c3316cb7c7f1e2a9
Niema Moshiri <niemamoshiri@gmail.com>
2018-02-08 12:21:19 -0800

    Fixed bug in Sequencing_Perfect

--- modules/Sequencing_Perfect.py
+++ modules/Sequencing_Perfect.py
@@ -6,7 +6,7 @@ Niema Moshiri 2016
 '''
 from Sequencing import Sequencing # abstract Sequencing class
 import FAVITES_GlobalContext as GC
-import os
+from os import getcwd,makedirs,chdir,listdir
 
 class Sequencing_Perfect(Sequencing):
     def cite():
@@ -16,14 +16,14 @@ class Sequencing_Perfect(Sequencing):
         pass
 
     def introduce_sequencing_error(node):
-        orig_dir = os.getcwd()
-        os.makedirs("error_prone_files/sequence_data", exist_ok=True)
-        os.chdir("error_prone_files/sequence_data")
-        l = [leaf for leaf in node.viruses()]
-        seq_data = [leaf.get_seq() for leaf in l]
-        labels = [leaf.get_label() for leaf in l]
-        f = open("seqs_%s.fastq" % node.get_name(), 'w')
-        for i in range(len(l)):
-            f.write("@%s\n%s\n+\n%s\n" % (labels[i], seq_data[i], '~'*len(seq_data[i])))
-        f.close()
-        os.chdir(orig_dir)
+        orig_dir = getcwd()
+        makedirs("error_prone_files/sequence_data", exist_ok=True)
+        chdir("error_prone_files/sequence_data")
+        for filename in listdir("%s/error_free_files/sequence_data" % GC.out_dir):
+            if filename.split('_')[1][1:] == node.get_name():
+                seqs = GC.parseFASTA(open("%s/error_free_files/sequence_data/%s" % (GC.out_dir,filename)))
+                f = open(filename.replace('.fasta','.fastq'), 'w')
+                for k in seqs:
+                    f.write("@%s\n%s\n+\n%s\n" % (k, seqs[k], '~'*len(seqs[k])))
+                f.close()
+        chdir(orig_dir)
