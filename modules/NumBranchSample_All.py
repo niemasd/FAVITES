1f58382aef7bad189c85b6bde6f76b8878ba6ca5
Niema Moshiri <niemamoshiri@gmail.com>
2018-03-13 18:36:19 -0700

    Added safety check for coalescent models of NodeEvolution

--- modules/NumBranchSample_All.py
+++ modules/NumBranchSample_All.py
@@ -13,7 +13,7 @@ class NumBranchSample_All(NumBranchSample):
         return GC.CITATION_FAVITES
 
     def init():
-        pass
+        assert "VirusTreeSimulator" not in str(MF.modules['NodeEvolution']), "Cannot use a coalescent model of evolution with NumBranchSample_All"
 
     def sample_num_branches(node, time):
         return float('inf')
