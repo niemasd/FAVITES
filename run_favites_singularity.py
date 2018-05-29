1e682d5fa09106ab20594f54ab30a51f7e7cbf3d
Niema Moshiri <niemamoshiri@gmail.com>
2018-05-28 20:35:57 -0700

    Fixed mounting issue when output directory is not on home filesystem

--- run_favites_singularity.py
+++ run_favites_singularity.py
@@ -5,12 +5,12 @@ FAVITES: FrAmework for VIral Transmission and Evolution Simulation
 import argparse
 import os
 from glob import glob
-from os import chdir,getcwd,makedirs,symlink
+from os import chdir,getcwd,makedirs,remove
 from os.path import abspath,expanduser,isdir,isfile
-from shutil import move
+from shutil import copyfile,move,rmtree
 from subprocess import call,check_output,CalledProcessError,DEVNULL,STDOUT
 from sys import platform,stderr,stdout
-from tempfile import NamedTemporaryFile,TemporaryDirectory
+from tempfile import TemporaryDirectory
 from warnings import warn
 from urllib.error import URLError
 from urllib.request import urlopen
@@ -45,8 +45,8 @@ parser.add_argument('-v', '--verbose', action="store_true", help="Print verbose 
 parser.add_argument('-u', '--update', required=True, nargs='*', help="Update Docker image (-u to pull newest version, -u <VERSION> to pull <VERSION>)")
 args = parser.parse_args()
 
-# check user args
-TMPDIR = TemporaryDirectory()
+# check core user args
+DELETE_AFTER = list()
 CONFIG = abspath(expanduser(args.config))
 assert isfile(CONFIG), "ERROR: Cannot open configuration file: %s" % CONFIG
 try:
@@ -59,46 +59,6 @@ if args.out_dir is not None:
     CONFIG_DICT['out_dir'] = args.out_dir
 assert 'out_dir' in CONFIG_DICT, "Parameter 'out_dir' is not in the configuration file!"
 OUTPUT_DIR = abspath(expanduser(CONFIG_DICT['out_dir']))
-if args.random_number_seed is not None:
-    if "random_number_seed" in CONFIG_DICT:
-        warn("Random number seed specified in command line (%d) and config file (%s). Command line will take precedence" % (args.random_number_seed, CONFIG_DICT['random_number_seed']))
-    CONFIG_DICT["random_number_seed"] = args.random_number_seed
-if "random_number_seed" not in CONFIG_DICT:
-    CONFIG_DICT["random_number_seed"] = ""
-CN_FILE = None
-if 'contact_network_file' in CONFIG_DICT:
-    CN_FILE = abspath(expanduser(CONFIG_DICT['contact_network_file']))
-    CONFIG_DICT['contact_network_file'] = '/FAVITES_MOUNT/%s' % CN_FILE.split('/')[-1]
-    symlink(CN_FILE, '%s/%s' % (TMPDIR.name, CN_FILE.split('/')[-1]))
-TN_FILE = None
-if 'transmission_network_file' in CONFIG_DICT:
-    TN_FILE = abspath(expanduser(CONFIG_DICT['transmission_network_file']))
-    CONFIG_DICT['transmission_network_file'] = '/FAVITES_MOUNT/%s' % TN_FILE.split('/')[-1]
-    symlink(TN_FILE, '%s/%s' % (TMPDIR.name, TN_FILE.split('/')[-1]))
-TREE_FILE = None
-if 'tree_file' in CONFIG_DICT:
-    TREE_FILE = abspath(expanduser(CONFIG_DICT['tree_file']))
-    CONFIG_DICT['tree_file'] = '/FAVITES_MOUNT/%s' % TREE_FILE.split('/')[-1]
-    symlink(TREE_FILE, '%s/%s' % (TMPDIR.name, TREE_FILE.split('/')[-1]))
-ERRORFREE_SEQ_FILE = None
-if 'errorfree_sequence_file' in CONFIG_DICT:
-    ERRORFREE_SEQ_FILE = abspath(expanduser(CONFIG_DICT['errorfree_sequence_file']))
-    CONFIG_DICT['errorfree_sequence_file'] = '/FAVITES_MOUNT/%s' % ERRORFREE_SEQ_FILE.split('/')[-1]
-    symlink(ERRORFREE_SEQ_FILE, '%s/%s' % (TMPDIR.name, ERRORFREE_SEQ_FILE.split('/')[-1]))
-TMP_CONFIG = NamedTemporaryFile('w')
-TMP_CONFIG.write(str(CONFIG_DICT))
-TMP_CONFIG.flush()
-symlink(TMP_CONFIG.name, '%s/USER_CONFIG.JSON' % TMPDIR.name)
-
-# pull the newest versioned Docker image (if applicable)
-if args.update is not None:
-    assert len(args.update) < 2, "More than one Docker image version specified. Must either specify just -u or -u <VERSION>"
-    if len(args.update) == 0:
-        tag = get_latest_version()
-    else:
-        tag = args.update[0]
-        assert tag not in INCOMPATIBLE, "Using incompatible version (%s). Singularity is only supported in FAVITES 1.1.7 onward"%tag
-    version = '%s:%s'%(DOCKER_IMAGE,tag)
 
 # create output directory
 try:
@@ -109,11 +69,55 @@ except:
         while len(response) == 0 or response[0] not in {'y','n'}:
             response = input("ERROR: Output directory exists. Overwrite? All contents will be deleted. (y/n) ").strip().lower()
             if response[0] == 'y':
-                from shutil import rmtree
                 rmtree(OUTPUT_DIR); makedirs(OUTPUT_DIR)
             else:
                 exit(-1)
-symlink(OUTPUT_DIR, '%s/OUTPUT_DIR' % TMPDIR.name)
+
+# check other user args
+if args.random_number_seed is not None:
+    if "random_number_seed" in CONFIG_DICT:
+        warn("Random number seed specified in command line (%d) and config file (%s). Command line will take precedence" % (args.random_number_seed, CONFIG_DICT['random_number_seed']))
+    CONFIG_DICT["random_number_seed"] = args.random_number_seed
+if "random_number_seed" not in CONFIG_DICT:
+    CONFIG_DICT["random_number_seed"] = ""
+CN_FILE = None
+if 'contact_network_file' in CONFIG_DICT:
+    CN_FILE = abspath(expanduser(CONFIG_DICT['contact_network_file']))
+    CONFIG_DICT['contact_network_file'] = '/FAVITES_MOUNT/%s' % CN_FILE.split('/')[-1]
+    DELETE_AFTER.append('%s/USER_CN.TXT' % OUTPUT_DIR)
+    copyfile(CN_FILE, DELETE_AFTER[-1])
+TN_FILE = None
+if 'transmission_network_file' in CONFIG_DICT:
+    TN_FILE = abspath(expanduser(CONFIG_DICT['transmission_network_file']))
+    CONFIG_DICT['transmission_network_file'] = '/FAVITES_MOUNT/%s' % TN_FILE.split('/')[-1]
+    DELETE_AFTER.append('%s/USER_TN.TXT' % OUTPUT_DIR)
+    copyfile(TN_FILE, DELETE_AFTER[-1])
+TREE_FILE = None
+if 'tree_file' in CONFIG_DICT:
+    TREE_FILE = abspath(expanduser(CONFIG_DICT['tree_file']))
+    CONFIG_DICT['tree_file'] = '/FAVITES_MOUNT/%s' % TREE_FILE.split('/')[-1]
+    DELETE_AFTER.append('%s/USER_TREE.TRE' % OUTPUT_DIR)
+    copyfile(TREE_FILE, DELETE_AFTER[-1])
+ERRORFREE_SEQ_FILE = None
+if 'errorfree_sequence_file' in CONFIG_DICT:
+    ERRORFREE_SEQ_FILE = abspath(expanduser(CONFIG_DICT['errorfree_sequence_file']))
+    CONFIG_DICT['errorfree_sequence_file'] = '/FAVITES_MOUNT/%s' % ERRORFREE_SEQ_FILE.split('/')[-1]
+    DELETE_AFTER.append('%s/USER_SEQS.FAS' % OUTPUT_DIR)
+    copyfile(ERRORFREE_SEQ_FILE, DELETE_AFTER[-1])
+DELETE_AFTER.append('%s/USER_CONFIG.JSON' % OUTPUT_DIR)
+TMP_CONFIG = open(DELETE_AFTER[-1],'w')
+TMP_CONFIG.write(str(CONFIG_DICT))
+TMP_CONFIG.close()
+
+# pull the newest versioned Docker image (if applicable)
+if args.update is not None:
+    assert len(args.update) < 2, "More than one Docker image version specified. Must either specify just -u or -u <VERSION>"
+    if len(args.update) == 0:
+        tag = get_latest_version()
+    else:
+        tag = args.update[0]
+        assert tag not in INCOMPATIBLE, "Using incompatible version (%s). Singularity is only supported in FAVITES 1.1.7 onward"%tag
+    version = '%s:%s'%(DOCKER_IMAGE,tag)
 
 # first pull Docker image as Singularity image
 pulled_image = expanduser('~/.favites/singularity-favites-%s.img'%tag)
@@ -131,7 +135,13 @@ if not isfile(pulled_image):
 
 # set up Docker command and run
 COMMAND =  ['singularity','run','-e']              # Singularity command
-COMMAND += ['-B',TMPDIR.name+':/FAVITES_MOUNT:rw'] # mount output directory
+COMMAND += ['-B',OUTPUT_DIR+':/FAVITES_MOUNT:rw']  # mount output directory
 COMMAND += [pulled_image]                          # Docker image
 call(COMMAND)
-TMP_CONFIG.close()
+
+# clean up temporary files
+for f in DELETE_AFTER:
+    remove(f)
+for f in glob('%s/OUTPUT_DIR/*'%OUTPUT_DIR):
+    move(f, '%s/%s'%(OUTPUT_DIR, f.split('/')[-1]))
+rmtree('%s/OUTPUT_DIR'%OUTPUT_DIR)
