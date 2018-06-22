ae3a78636e1877788cb3a407096e7ab1dd6f265a
Niema Moshiri <niemamoshiri@gmail.com>
2018-06-22 00:50:42 -0700

    Added TreeUnit_AutocorrelatedExponential

---
+++ modules/TreeUnit_AutocorrelatedExponential.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2017
+
+"TreeUnit" module, where each branch's length (in time units) is multiplied by
+a rate that is sampled from an exponential distribution whose mean is the rate
+of the parent branch.
+'''
+from TreeUnit import TreeUnit
+import FAVITES_GlobalContext as GC
+
+class TreeUnit_AutocorrelatedExponential(TreeUnit):
+    def cite():
+        return [GC.CITATION_FAVITES, GC.CITATION_NUMPY, GC.CITATION_TREESWIFT]
+
+    def init():
+        try:
+            global exponential
+            from numpy.random import exponential
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading NumPy. Install with: pip3 install numpy"
+        try:
+            global read_tree_newick
+            from treeswift import read_tree_newick
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading TreeSwift. Install with: pip3 install treeswift"
+        GC.tree_rate_R0 = float(GC.tree_rate_R0)
+        assert GC.tree_rate_R0 > 0, "tree_rate_R0 must be positive"
+
+    def time_to_mutation_rate(tree):
+        if not hasattr(GC,"NUMPY_SEEDED"):
+            from numpy.random import seed as numpy_seed
+            numpy_seed(seed=GC.random_number_seed)
+            GC.random_number_seed += 1
+            GC.NUMPY_SEEDED = True
+        t = read_tree_newick(tree)
+        for node in t.traverse_preorder():
+            if node.is_root():
+                node.rate = GC.tree_rate_R0
+            else:
+                node.rate = exponential(scale=node.parent.rate)
+            if node.edge_length is not None:
+                node.edge_length *= node.rate
+        return str(t)
