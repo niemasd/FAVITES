81db2aedfaff97847a2d43c1da5a4607d3170aa1
Niema Moshiri <niemamoshiri@gmail.com>
2018-08-18 14:44:07 -0700

    Added TreeUnit_Normal

---
+++ modules/TreeUnit_Normal.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+'''
+Niema Moshiri 2017
+
+"TreeUnit" module, where each branch's length (in time units) is multiplied by
+a rate that is sampled from a user-parameterized Normal (Gaussian) distribution.
+'''
+from TreeUnit import TreeUnit
+import FAVITES_GlobalContext as GC
+
+class TreeUnit_Normal(TreeUnit):
+    def cite():
+        return [GC.CITATION_FAVITES, GC.CITATION_NUMPY, GC.CITATION_TREESWIFT]
+
+    def init():
+        try:
+            global normal
+            from numpy.random import normal
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading NumPy. Install with: pip3 install numpy"
+        try:
+            global read_tree_newick
+            from treeswift import read_tree_newick
+        except:
+            from os import chdir
+            chdir(GC.START_DIR)
+            assert False, "Error loading TreeSwift. Install with: pip3 install treeswift"
+        GC.tree_rate_mean = float(GC.tree_rate_mean)
+        GC.tree_rate_stdev = float(GC.tree_rate_stdev)
+        assert GC.tree_rate_stdev > 0, "tree_rate_stdev must be positive"
+
+    def time_to_mutation_rate(tree):
+        if not hasattr(GC,"NUMPY_SEEDED"):
+            from numpy.random import seed as numpy_seed
+            numpy_seed(seed=GC.random_number_seed)
+            GC.random_number_seed += 1
+            GC.NUMPY_SEEDED = True
+        t = read_tree_newick(tree)
+        for node in t.traverse_preorder():
+            if node.edge_length is not None:
+                node.edge_length *= normal(loc=GC.tree_rate_mean,scale=GC.tree_rate_stdev)
+        return str(t)
